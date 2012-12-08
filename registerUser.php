ddc86650d9b8a02e9759c6c9425b0a68d95c421e
Carl Jamilkowski <cdj255@nyu.edu>
2012-12-08 17:17:48 -0500

    Switching logic so that usernames are recycled

--- registerUser.php
+++ registerUser.php
@@ -8,35 +8,47 @@ if (isset($_POST['UserName']) && (strlen(trim($_POST['UserName'])) > 0))
 	$con = mysql_connect($dbServerName,$dbUserName,$dbUserPassword);
 	if (!$con)
 	{
-		die('Could not connect: ' . mysql_error());
+		$errorText = mysql_error();
+		header('HTTP/1.1 500 '.$errorText);
+		die('Could not connect: ' . $errorText);
 	}
 	
 	mysql_select_db($dbName, $con);
-	
-	$sql = "INSERT INTO `Users` (`UserName`) VALUES ('".$userName."')";
-	
-	if (!mysql_query($sql,$con))
-	{
-		die('Error: ' . mysql_error());
 	}
-	else
+	
+	$userID = -1;
+	$num_rows = -1;
+		
+	do
 	{
-		// Get the user ID for the user we just registered
-		$userID = mysql_insert_id();
-		$sql = "SELECT * FROM `Users` WHERE `UserID`=".$userID;
+		$sql = "SELECT * FROM `Users` WHERE `UserName`='".$userName."' ORDER BY `UserID` DESC";
 		$result = mysql_query($sql);
-		$row = mysql_fetch_array($result);
-		if($row['UserName'] != $userName)
+		if(!$result)
 		{
-			// semi-workaround for potential race condition
-			$sql = "SELECT * FROM `Users` WHERE `UserID`<".$userID." AND `UserName`='".$userName."' ORDER BY `UserID` DESC";
-			$result = mysql_query($sql);
+			$errorText = mysql_error();
+			header('HTTP/1.1 500 '.$errorText);
+			die('Error: ' . $errorText);
+		}
+		$num_rows = mysql_num_rows($result);
+		if ($num_rows > 0)
+		{
 			$row = mysql_fetch_array($result);
 			$userID = $row['UserID'];
 		}
-		echo $userID;
-	}
+		else
+		{
+			$sql = "INSERT INTO `Users` (`UserName`) VALUES ('".$userName."')";
+			if (!mysql_query($sql,$con))
+			{
+				$errorText = mysql_error();
+				header('HTTP/1.1 500 '.$errorText);
+				die('Error: ' . $errorText);
+			}
+		}
+	} while($num_rows <= 0);
 	
 	mysql_close($con);
+	
+	echo $userID;	
 }
 ?>
